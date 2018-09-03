99776ef60342227ca8e9e5b34a9321b9d7e67844
Nauel G <nauelg@MacBook-Pro-de-Nauel.local>
2018-09-04 00:07:04 +0200

    Fix error component

--- src/components/header/header-component.js
+++ src/components/header/header-component.js
@@ -2,7 +2,7 @@ import './header-styles.scss';
 
 export const makeHeader = ({ title }) => {
     const header = document.createElement('header');
-    header.innerHTML = `<h1 class = "${title}" >Keep </h1>`;
+    header.innerHTML = `<h1 class ="title">${ title }</h1>`;
     return header;
 };
 
