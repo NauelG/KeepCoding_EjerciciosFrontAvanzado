7d5259a9a7ddecbaf0b6b839da9391fe80e19731
Nauel G <nauelg@MacBook-Pro-de-Nauel.local>
2018-09-04 23:07:44 +0200

    File utils

---
+++ src/utils/utils.js
@@ -0,0 +1,10 @@
+export const appendComponent = (parent, components) => {
+
+    components.forEach(component => {
+        parent.appendChild(component);
+    });
+};
+
+export default {
+    appendComponent
+};
