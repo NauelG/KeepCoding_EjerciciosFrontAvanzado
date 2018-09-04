aeed21307c53c8898330e326d0b51ea3c24a545e
Nauel G <nauelg@MacBook-Pro-de-Nauel.local>
2018-09-04 23:07:52 +0200

    Footer component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import './index.scss';
 import { makeHeader } from 'components/header/header-component';
-import { makeImage } from 'components/image/image-component';
+import { appendComponent } from 'utils/utils';
+import { makeFooter } from 'components/footer/footer-component';
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.body.appendChild(makeHeader({ title: 'Keep Coding' }));
-    document.body.appendChild(makeImage());
+    var components = [
+        makeHeader({ title: 'Keep Coding' }),
+        makeFooter()
+    ]
+
+    appendComponent(document.body, components);
 });
