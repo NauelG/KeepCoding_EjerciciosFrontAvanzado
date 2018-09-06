f01c8b0de5be0dbfa67c608f5322af890edb66ad
Nauel G <nauelg@MacBook-Pro-de-Nauel.local>
2018-09-06 20:57:42 +0200

    Cambios al dia con el instructor

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var merge = require('webpack-merge');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var commonConfig = {
     entry: path.join(__dirname, 'src', 'index'),
@@ -24,23 +26,62 @@ var commonConfig = {
                 use: "babel-loader"
             },
             {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader'
+                    },
+                ],
             }
         ]
-    }
+    },
+    resolve: {
+        alias: {
+            components: path.resolve(__dirname, 'src', 'components'),
+            assets: path.resolve(__dirname, 'src', 'assets'),
+            styles: path.resolve(__dirname, 'src', 'styles'),
+            utils: path.resolve(__dirname, 'src', 'utils'),
+            data: path.resolve(__dirname, 'src', 'data')
+        }
+    },
 };
 
 var devConfig = {
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }, ]
+    },
     devServer: {
-        open: true,
         overlay: true,
-        port: 3000,
-        hot: true
-    }
+        port: 3000
+    },
 };
 
-var prodConfig = {};
+var prodConfig = {
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ]
+        }, ]
+    },
+};
 
 module.exports = (env, argv) =>
     argv.mode === 'development' ?
