93f9c604c1c399adf9a5318f43b825a745684fb3
Nauel G <nauelg@MacBook-Pro-de-Nauel.local>
2018-09-10 21:07:15 +0200

    Correcciones

--- src/components/header/header-component.js
+++ src/components/header/header-component.js
@@ -1,10 +1,8 @@
-import './header-styles.scss';
-
 export const updateHeader = ({ title }) => {
-  const titleElement = document.getElementById('title');
-  titleElement.innerHTML = title;
+    const titleElement = document.getElementById('title');
+    titleElement.innerHTML = title;
 };
 
 export default {
-  updateHeader
+    updateHeader
 };
