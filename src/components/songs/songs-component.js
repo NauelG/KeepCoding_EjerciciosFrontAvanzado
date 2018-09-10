c49506a69bac4bd70d666e78a9e0f5cddfa90ad2
Nauel G <nauelg@MacBook-Pro-de-Nauel.local>
2018-09-10 21:19:52 +0200

    Webpack pages function

--- src/components/songs/songs-component.js
+++ src/components/songs/songs-component.js
@@ -2,19 +2,19 @@ import { appendComponent } from 'utils/utils';
 import { createSong } from 'components/song/song-component';
 
 export const createSongs = () => {
-  const songs = document.getElementById('songs');
-  fetch('http://localhost:8000/songs').then(
-    (response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      console.warn('error', response.status);
-      return null;
-    }
-  ).then((songsJSON) => {
-    appendComponent(songs,
-      songsJSON.map(songData => createSong(songData)));
-  });
+    const songs = document.getElementById('songs');
+    fetch('http://localhost:3001/songs').then(
+        (response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            console.warn('error', response.status);
+            return null;
+        }
+    ).then((songsJSON) => {
+        appendComponent(songs,
+            songsJSON.map(songData => createSong(songData)));
+    });
 };
 
 export default createSongs;
--- webpack.config.js
+++ webpack.config.js
@@ -6,119 +6,116 @@ var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 var CriticalPlugin = require('webpack-plugin-critical').CriticalPlugin;
 
+var page = function({ title, template, chunks, filename }) {
+    return new HtmlWebpackPlugin({
+        title: title,
+        template: template,
+        chunks: chunks,
+        minify: {
+            collapseWhitespace: true
+        },
+        filename: filename
+    })
+}
+
 var commonConfig = {
-  entry: {
-    songs: path.join(__dirname, 'src', 'pages', 'songs', 'index'),
-    contact: path.join(__dirname, 'src', 'pages', 'contact', 'index')
-  },
-  output: {
-    filename: '[name][hash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new HtmlWebpackPlugin(
-      {
-        title: 'Play',
-        template: path.join(__dirname, 'src', 'pages', 'songs', 'index.html'),
-        chunks: ['songs'],
-        minify: {
-          collapseWhitespace: true
-        },
-        filename: path.resolve(__dirname, 'dist', 'index.html')
-      }
-    ),
-    new HtmlWebpackPlugin(
-      {
-        title: 'Contact',
-        template: path.join(__dirname, 'src', 'pages', 'contact', 'index.html'),
-        chunks: ['contact'],
-        minify: {
-          collapseWhitespace: true
-        },
-        filename: path.resolve(__dirname, 'dist', 'contact', 'index.html')
-      }
-    )
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: "babel-loader"
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              name: "assets/[name].[hash].[ext]",
+    entry: {
+        songs: path.join(__dirname, 'src', 'pages', 'songs', 'index'),
+        contact: path.join(__dirname, 'src', 'pages', 'contact', 'index')
+    },
+    output: {
+        filename: '[name][hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        page({
+            title: 'Play',
+            template: path.join(__dirname, 'src', 'pages', 'songs', 'index.html'),
+            chunks: ['songs'],
+            filename: path.resolve(__dirname, 'dist', 'index.html')
+        }),
+        page({
+            title: 'Contact',
+            template: path.join(__dirname, 'src', 'pages', 'contact', 'index.html'),
+            chunks: ['contact'],
+            filename: path.resolve(__dirname, 'dist', 'contact', 'index.html')
+        })
+    ],
+    module: {
+        rules: [{
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
             },
-          },
-        ],
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      components: path.resolve(__dirname, 'src', 'components'),
-      assets: path.resolve(__dirname, 'src', 'assets'),
-      styles: path.resolve(__dirname, 'src', 'styles'),
-      utils: path.resolve(__dirname, 'src', 'utils'),
-      data: path.resolve(__dirname, 'src', 'data')
-    }
-  },
-  devtool: 'source-map'
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            name: "assets/[name].[hash].[ext]",
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            components: path.resolve(__dirname, 'src', 'components'),
+            assets: path.resolve(__dirname, 'src', 'assets'),
+            styles: path.resolve(__dirname, 'src', 'styles'),
+            utils: path.resolve(__dirname, 'src', 'utils'),
+            data: path.resolve(__dirname, 'src', 'data')
+        }
+    },
+    devtool: 'source-map'
 };
 
 var devConfig = {
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-    ]
-  },
-  devServer: {
-    overlay: true,
-    port: 3000
-  },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }, ]
+    },
+    devServer: {
+        overlay: true,
+        port: 3000
+    },
 };
 
 var prodConfig = {
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].[hash].css'
-    }),
-    new CleanWebpackPlugin(['dist']),
-    new CriticalPlugin({
-      src: path.join(__dirname, 'src', 'pages', 'songs', 'index.html'),
-      inline: true,
-      minify: true,
-      dest: path.join(__dirname, 'dist', 'index.html')
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-    ]
-  },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css'
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new CriticalPlugin({
+            src: path.join(__dirname, 'src', 'pages', 'songs', 'index.html'),
+            inline: true,
+            minify: true,
+            dest: path.join(__dirname, 'dist', 'index.html')
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ]
+        }, ]
+    },
 };
 
 module.exports = (env, argv) =>
-  argv.mode === 'development' ?
+    argv.mode === 'development' ?
     merge(commonConfig, devConfig) :
     merge(commonConfig, prodConfig);
