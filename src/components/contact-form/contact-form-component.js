0613877e1bed60e1f0250c2193a9dec1a6ee313c
Nauel G <nauelg@MacBook-Pro-de-Nauel.local>
2018-09-13 19:51:35 +0200

    Formulario añadido y modulos reinstalados

---
+++ src/components/contact-form/contact-form-component.js
@@ -0,0 +1,52 @@
+
+import MessageService from 'services/message-service';
+
+const addErrorValidation = (formInputs) => {
+  for (let i = 0; i < formInputs.length; i += 1) {
+    const input = formInputs[i];
+    input.addEventListener('blur', () => {
+      if (!input.checkValidity()) {
+        input.classList.add('error');
+      } else {
+        input.classList.remove('error');
+      }
+    });
+  }
+};
+
+const getFormData = (formInputs) => {
+  const formData = {};
+  for (let i = 0; i < formInputs.length; i += 1) {
+    const input = formInputs[i];
+    formData[input.name] = input.value;
+  }
+  return formData;
+};
+
+export const updateContactForm = () => {
+  const contactForm = document.getElementById('contact-form');
+  const submitFormButton = document.getElementById('contact-form-submit');
+  const formInputs = contactForm.getElementsByClassName('contact-input');
+  const notice = document.getElementById('notice');
+
+  addErrorValidation(formInputs);
+
+  submitFormButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    submitFormButton.disable = true;
+    contactForm.reportValidity();
+    if (contactForm.checkValidity()) {
+      const MessageServiceInstance = new MessageService();
+      MessageServiceInstance.postMessage(getFormData(formInputs)).then(
+        (response) => {
+          if (response === true) {
+            notice.innerHTML = 'Your message has been sent';
+          }
+        }
+      );
+      submitFormButton.disable = false;
+    }
+  });
+};
+
+export default updateContactForm;
